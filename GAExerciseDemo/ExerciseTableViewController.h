b1e6d42a72ebda2176759dfc48529e514d7d3597
Chris Linseman <clinseman@gmail.com>
2012-09-12 21:26:38 +0100

    added ExerciseTableViewController

---
+++ GAExerciseDemo/ExerciseTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ExerciseTableViewController.h
+//  GAExerciseDemo
+//
+//  Created by Chris Linseman on 12/09/2012.
+//  Copyright (c) 2012 Chris Linseman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ExerciseTableViewController : UITableViewController
+
+@end
