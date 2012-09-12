db8892c753358b40c83e95e7964fc428eb115587
Chris Linseman <clinseman@gmail.com>
2012-09-12 21:30:59 +0100

    Set up root view controller

--- GAExerciseDemo/AppDelegate.m
+++ GAExerciseDemo/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "ExerciseTableViewController.h"
 
 @implementation AppDelegate
 
@@ -14,6 +15,10 @@
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
+    
+    ExerciseTableViewController *exerciseTableViewController = [[ExerciseTableViewController alloc] init];
+    [self.window setRootViewController:exerciseTableViewController];
+    
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
