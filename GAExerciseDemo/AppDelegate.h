8ad8ce561e3295ab139ef8eed63b2d38e1f18d46
Chris Linseman <clinseman@gmail.com>
2012-09-12 21:13:10 +0100

    Initial Commit

---
+++ GAExerciseDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GAExerciseDemo
+//
+//  Created by Chris Linseman on 12/09/2012.
+//  Copyright (c) 2012 Chris Linseman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
